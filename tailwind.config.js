bb7945b4f2a775765d5f9cdb308dbfc779d47e5b
aimanur99 <aimanurrahman1999@gmail.com>
2024-11-04 00:21:30 +0600

    Added the first commit in the html file

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+//tailwind.config.cdn
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: ["./src/**/*.{html,js}"],
+    theme: {
+      extend: {},
+    },
+    plugins: [],
+  }
